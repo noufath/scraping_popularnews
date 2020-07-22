5da42830b21c87f26f481f8514140f5b7323d955
Dany <danychristianto@gmail.com>
2020-07-22 15:14:44 +0700

    Remove main.py

--- templates/main.py
+++
@@ -1,8 +0,0 @@
-from getdata import GetData
-
-soup = GetData("https://www.detik.com/terpopuler?","{'tag_from':'wp_cb_mostPopular_more'}").html_parser()
-
-popular_area = soup.find(attrs={'class':'grid-row list-content'})
-
-#print(soup.prettify())
-print(popular_area.prettify())
